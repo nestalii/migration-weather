560ad3b5aa30f40854da79ad8dfd97a23c826a0d
Anastasiia Rynkevych <nestalirunkevich@gmail.com>
2025-05-16 22:11:34 +0300

    feat: create wind measurements table
    - modify weather table
    - create second migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .base import Base
 from .weather import Weather, WindDirection
+from .wind_measurements import WindMeasurements
 from app.utils import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
--- app/models/weather.py
+++ app/models/weather.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, Float, String, Date, Time, Enum
-import enum
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from .base import Base
+import enum
 
 class WindDirection(enum.Enum):
     N = "N"
@@ -17,8 +18,5 @@ class Weather(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     country = Column(String, nullable=False)
-    wind_degree = Column(Integer)
-    wind_kph = Column(Float)
-    wind_direction = Column(Enum(WindDirection))
-    last_updated = Column(Date)
-    sunrise = Column(Time)
+
+    wind_measurements = relationship("WindMeasurements", back_populates="weather", cascade="all, delete")
