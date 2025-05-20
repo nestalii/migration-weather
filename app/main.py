697b699be6b03f08abe592cb54c66e03eae4a06b
Anastasiia Rynkevych <nestalirunkevich@gmail.com>
2025-05-20 18:38:32 +0300

    feat: impl console interface

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 from app.utils import read_weather_csv, fill_weather
+from app.utils.interface import get_weather_by_country_and_date
 
-df = read_weather_csv("data/weather.csv")
-fill_weather(df)
+# df = read_weather_csv("data/weather.csv")
+# fill_weather(df)
+
+country = input("Enter the country: ").strip()
+date = input("Enter the date (YYYY-MM-DD): ").strip()
+
+result = get_weather_by_country_and_date(country, date)
+print(result)
