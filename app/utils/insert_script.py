0b897dc2aa41ef8062b322916c8f03af71d5f98a
Anastasiia Rynkevych <nestalirunkevich@gmail.com>
2025-05-20 18:23:39 +0300

    fix: insert_script

--- app/utils/insert_script.py
+++ app/utils/insert_script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
 from app.models import Weather, SessionLocal, WindDirection, WindMeasurements
 
 def read_weather_csv(file_path):
@@ -24,11 +25,14 @@ def fill_weather(df: pd.DataFrame):
 
     try:
         for _, row in df.iterrows():
-            weather = Weather(
-                country=row['country']
-            )
-            session.add(weather)
-            session.commit()
+            try:
+                weather = session.query(Weather).filter_by(country=row['country']).one()
+            except NoResultFound:
+                weather = Weather(
+                    country=row['country']
+                )
+                session.add(weather)
+                session.commit()
 
             wind_measurement = WindMeasurements(
                 weather_id=weather.id,
@@ -37,7 +41,7 @@ def fill_weather(df: pd.DataFrame):
                 wind_direction=WindDirection[row['wind_direction']] if row['wind_direction'] in WindDirection.__members__ else None,
                 last_updated=pd.to_datetime(row['last_updated']).date() if not pd.isna(row['last_updated']) else None,
                 sunrise=pd.to_datetime(row['sunrise']).time() if not pd.isna(row['sunrise']) else None,
-                go_outside = should_go_outside(float(row['wind_kph'])) if not pd.isna(row['wind_kph']) else None
+                go_outside=should_go_outside(float(row['wind_kph'])) if not pd.isna(row['wind_kph']) else None
             )
             session.add(wind_measurement)
 
